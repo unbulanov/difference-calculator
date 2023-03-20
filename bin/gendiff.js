24eb6418481ea71757e676409806a7c4b9782197
Ekaterina Ivanova <unbulanov@gmail.com>
2023-03-20 22:16:46 +0300

    command parse

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,3 +6,5 @@ const program = new Command();
 program
 .description('Compares two configuration files and shows a difference.')
 .version('0.0.1', '-V, --version', 'output the version number')
+
+program.parse();
