255a9f3ee482b0672333e678e1e3f931bfe0061f
Ekaterina Ivanova <unbulanov@gmail.com>
2023-03-23 18:35:54 +0300

    add func diff

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import { readFileSync } from 'fs';
+import _ from 'lodash';
 
-const genDiff = (filepath1, filepath2) => {
+const genDiff = (data1, data2) => {
+  const keys1 = _.keys(data1);
+  const keys2 = _.keys(data2);
+  const keys = _.union(keys1, keys2);
+};
+
+const readFile = (filepath1, filepath2) => {
   const content1 = readFileSync(filepath1, 'utf-8');
   const parseContent1 = JSON.parse(content1);
   const content2 = readFileSync(filepath2, 'utf-8');
