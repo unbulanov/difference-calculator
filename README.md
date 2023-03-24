e19ce02f8f44c54c1b6e4140e944b27ba01e6514
Ekaterina Ivanova <unbulanov@gmail.com>
2023-03-24 22:59:41 +0300

    add eslint yml

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 [![Actions Status](https://github.com/unbulanov/frontend-project-46/workflows/hexlet-check/badge.svg)](https://github.com/unbulanov/frontend-project-46/actions)
 
 1. genDiff:
+
+
 <a href="https://asciinema.org/a/t4HZEAAW0rHuLjJJGcmlI7zu6" target="_blank"><img src="https://asciinema.org/a/t4HZEAAW0rHuLjJJGcmlI7zu6.svg" /></a>
--- src/index.js
+++ src/index.js
@@ -9,54 +9,54 @@ const getInfo = (data1, data2) => {
     const value1 = data1[key];
     const value2 = data2[key];
     if ((value1 && value2) && (value1 !== value2)) {
-        return {
-            type: 'changed',
-            key,
-            value1,
-            value2,
-        };
+      return {
+        type: 'changed',
+        key,
+        value1,
+        value2,
+      };
     }
     if (!_.has(data2, key)) {
-        return {
-            type: 'deleted',
-            key,
-            value: value1,
-        };
+      return {
+        type: 'deleted',
+        key,
+        value: value1,
+      };
     }
     if (!_.has(data1, key)) {
-        return {
-            type: 'added',
-            key,
-            value: value2,
-        };
+      return {
+        type: 'added',
+        key,
+        value: value2,
+      };
     }
     return {
-        type: 'unchanged',
-        key,
-        value1: value1,
+      type: 'unchanged',
+      key,
+      value1,
     };
   });
   return result;
 };
 
 const getDiff = (diffArr) => {
-    const result = diffArr.map((diff) => {
-        const diffType = diff.type;
-        switch (diffType) {
-            case 'deleted':
-                return `  - ${ diff.key }: ${ diff.value }`;
-            case 'unchanged':
-                return `    ${ diff.key }: ${ diff.value1 }`;
-            case 'changed':
-                return (`  - ${ diff.key }: ${ diff.value1 } \n  + ${ diff.key }: ${ diff.value2 }`);
-            case 'added':
-                return `  + ${ diff.key }: ${ diff.value }`;
-            default:
-                return null;
-        }
-    });
-    return `{\n${result.join('\n')}\n}`;
-}
+  const result = diffArr.map((diff) => {
+    const diffType = diff.type;
+    switch (diffType) {
+      case 'deleted':
+        return `  - ${diff.key}: ${diff.value}`;
+      case 'unchanged':
+        return `    ${diff.key}: ${diff.value1}`;
+      case 'changed':
+        return (`  - ${diff.key}: ${diff.value1} \n  + ${diff.key}: ${diff.value2}`);
+      case 'added':
+        return `  + ${diff.key}: ${diff.value}`;
+      default:
+        return null;
+    }
+  });
+  return `{\n${result.join('\n')}\n}`;
+};
 
 const getAbsolutePath = (filepath) => path.resolve(process.cwd(), '__fixtures__', filepath);
 
@@ -65,6 +65,5 @@ const readFile = (filepath) => readFileSync(getAbsolutePath(filepath), 'utf-8');
 const getParseFile = (filepath) => JSON.parse(readFile(filepath));
 
 const genDiff = (filepath1, filepath2) => (getDiff(getInfo(getParseFile(filepath1), getParseFile(filepath2))));
-
 
 export default genDiff;
