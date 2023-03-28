21e3694fac78545de4f8a5413d07621538801485
Ekaterina Ivanova <unbulanov@gmail.com>
2023-03-28 19:55:32 +0300

    upd gitignore

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,17 +2,19 @@ import { readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 import genDiff from '../src/index.js';
-
+  
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
-
 const fileOutputJson = readFile('fileOutputJsonDiff.txt');
+const file1 = getFixturePath('file1.json');
+const file2 = getFixturePath('file2.json');
 
-const first = getFixturePath('file1.json');
-const second = getFixturePath('file2.json');
-
-test('difference test 1', () => {
-  expect(genDiff(first, second)).toEqual(fileOutputJson);
+describe('comparing files', () => {
+    test('gendiff two files json', () => {
+        expect(genDiff(file1, file2)).toEqual(fileOutputJson);
+    });
 });
+
