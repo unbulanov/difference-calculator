7057288d5f7a3df296098549c9c4a2c5e309362f
Ekaterina Ivanova <unbulanov@gmail.com>
2023-04-02 21:50:42 +0300

    refactoring buildtree

--- src/buildTree.js
+++ src/buildTree.js
@@ -4,25 +4,28 @@ const buildTree = (data1, data2) => {
   const commonKeys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
 
   const result = commonKeys.map((key) => {
-    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
-      return { type: 'nested', key: `${key}`, children: buildTree(data1[key], data2[key]) };
+    const val1 = data1[key];
+    const val2 = data2[key];
+
+    if (_.isPlainObject(val1) && _.isPlainObject(val2)) {
+      return { type: 'nested', key: `${key}`, children: buildTree(val1, val2) };
     }
 
-    if (!Object.hasOwn(data1, key)) {
-      return { type: 'added', key: `${key}`, value: data2[key] };
+    if (!_.has(data1, key)) {
+      return { type: 'added', key: `${key}`, value: val2 };
     }
 
-    if (!Object.hasOwn(data2, key)) {
-      return { type: 'deleted', key: `${key}`, value: data1[key] };
+    if (!_.has(data2, key)) {
+      return { type: 'deleted', key: `${key}`, value: val1 };
     }
 
-    if (!_.isEqual(data1[key], data2[key])) {
+    if (!_.isEqual(val1, val2)) {
       return {
-        type: 'changed', key: `${key}`, value1: data1[key], value2: data2[key],
+        type: 'changed', key: `${key}`, value1: val1, value2: val2,
       };
     }
 
-    return { type: 'unchanged', key: `${key}`, value: data1[key] };
+    return { type: 'unchanged', key: `${key}`, value: val1 };
   });
 
   return result;
